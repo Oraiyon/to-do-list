0e6b7e9b31a65857e024029c6cbee8858793c1b3
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-09-06 15:10:11 +0800

    added function to create project obj

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
 import "./styles/main.css";
 import "./styles/buttons.css";
+import { addProjectButton } from "./scripts/sideBar";
+
+const makeProject = (title, description) => {
+    return {title, description};
+};
