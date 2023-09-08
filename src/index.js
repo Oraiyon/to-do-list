f6bb7914fe09857999db5a28e8fe54944f0eaa61
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-09-08 15:15:29 +0800

    exported html elements and imported ()s

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import "./styles/main.css";
+import "./styles/sideBar.css";
+import "./styles/modal.css";
+import { addProjectButton, closeModalButton, submitModalButton } from "./scripts/modal";
+import { displayProjects } from "./scripts/sideBar";
+import { checkProjectEmpty, displayCurrentProject } from "./scripts/main";
+
+export const projectCards= document.querySelector(".projectCards");
+export const addTask= document.querySelector(".addTask");
+export const mainPage= document.querySelector(".main");
+export const projectName= document.querySelector(".projectName");
+export const projectDescription= document.querySelector(".projectDescription");
+export const tasks= document.querySelector(".tasks");
+export const taskForm= document.querySelector(".taskForm");
+export const cancelTaskForm= document.querySelector(".cancelTask");
+
+export const projects=[];
+
+export const makeProject = (title, description) => {
+    return {title, description};
+};
+
+checkProjectEmpty();
+
+//Try removing this () from modal.js
+export const addProjectToProjects= (project) => {
+    projects.push(project);
+    displayProjects();
+    displayCurrentProject();
+    checkProjectEmpty();
+};
