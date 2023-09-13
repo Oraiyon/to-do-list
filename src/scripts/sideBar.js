55251d1466a28ef79bdde775b9cf4ae30540c995
Oliver Trinidad <ocmtrinidad@gmail.com>
2023-09-13 12:05:15 +0800

    displayProject() changes project.title when edited

--- src/scripts/sideBar.js
+++ src/scripts/sideBar.js
@@ -1,10 +1,13 @@
-import { projectCards, projects } from "../index";
+import { projectCards, projects, submitEdit } from "../index";
 import { modalAdd } from "./modal";
 
 export const displayProjects = () => {
     projects.forEach(project => {
         const card= document.createElement("button");
         modalAdd.addEventListener("click", () => {
+            card.remove();
+        });
+        submitEdit.addEventListener("click", () => {
             card.remove();
         });
         card.innerText= project.title;
